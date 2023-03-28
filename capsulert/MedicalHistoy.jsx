833c9bf7d3f03d1323793aa972df109bbbb6f892
holly-th <holly.thomas135@gmail.com>
2023-03-28 17:03:30 +0100

    completed adding history, working on database

--- capsulert/MedicalHistoy.jsx
+++ capsulert/MedicalHistoy.jsx
@@ -7,12 +7,20 @@ import {
   FlatList,
 } from "react-native";
 import React, { useState } from "react";
+// import { getDatabase, ref, set } from "firebase/database";
 
+// function testData(userUID, name, email) {
+//   const db = getDatabase();
+//   set(ref(db, "/users/" + userUID), {
+//     username: name,
+//     email: email,
+//   });
+// }
+// testData(1, "holly", "holly@test.com");
 const MedicalHistory = () => {
   const [newInput, setNewInput] = useState("");
   const [list, setList] = useState([]);
-  // console.log(text);
-  // console.log(list);
+
   const handleSubmit = (newInput) => {
     setList((currentList) => {
       return [...currentList, newInput];
@@ -20,11 +28,18 @@ const MedicalHistory = () => {
     setNewInput("");
   };
   const handleRemove = (item) => {
-    // console.log(item);
-    // console.log(...list);
     const removedItemArray = list.filter((e) => e !== item);
     setList(removedItemArray);
   };
+
+  // const handleSave = () => {
+  //   let data = {
+  //     items: newInput,
+  //   };
+  //   let ref = database.ref(`/users/` + userUID);
+  //   ref.push(data);
+  // };
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Medical History</Text>
@@ -37,10 +52,11 @@ const MedicalHistory = () => {
         value={newInput}
       />
 
-      <TouchableOpacity style={styles.addButton}>
-        <Text style={styles.add} onPress={() => handleSubmit(newInput)}>
-          Add
-        </Text>
+      <TouchableOpacity
+        style={styles.addButton}
+        onPress={() => handleSubmit(newInput)}
+      >
+        <Text style={styles.add}>Add</Text>
       </TouchableOpacity>
       {console.log(list)}
       <Text style={styles.yourHistory}>Your medical history</Text>
@@ -48,10 +64,19 @@ const MedicalHistory = () => {
       <FlatList
         style={styles.flatlist}
         data={list}
-        renderItem={({ item }) => {
-          <Text>{item}</Text>;
-        }}
+        renderItem={({ item }) => (
+          <TouchableOpacity style={styles.itemBox}>
+            <Text style={styles.item}>{item}</Text>
+            <Text
+              style={styles.removeButton}
+              onPress={() => handleRemove(item)}
+            >
+              Remove
+            </Text>
+          </TouchableOpacity>
+        )}
       ></FlatList>
+      <TouchableOpacity onPress={() => handleSave}>Save</TouchableOpacity>
     </View>
   );
 };
@@ -87,7 +112,6 @@ const styles = StyleSheet.create({
   yourHistory: {
     fontSize: 20,
     marginTop: 20,
-    // textDecorationLine: "underline",
     marginLeft: 20,
   },
   boxList: {
@@ -102,31 +126,33 @@ const styles = StyleSheet.create({
   removeButton: {
     borderColor: "#000000",
     borderWidth: 2,
+    margin: 10,
+    borderRadius: 15,
+    paddingTop: 5,
+    paddingBottom: 5,
+    width: 70,
+    alignItems: "center",
+    alignSelf: "flex-end",
+    marginRight: 40,
+    textAlign: "center",
+    verticalAlign: "center",
   },
   flatlist: {
     backgroundColor: "#ADD8E6",
+    marginTop: 10,
+    borderColor: "#000000",
+    borderWidth: 1,
+    margin: 10,
+  },
+  item: {
+    paddingLeft: 20,
+    paddingTop: 10,
+    textAligne: "center",
+  },
+  itemBox: {
+    borderBottomColor: "#000000",
+    borderWidth: 0.5,
+    alignContent: "center",
   },
 });
 export default MedicalHistory;
-{
-  /* {list
-            ? list.map((item) => {
-                return (
-                  <View>
-                    <Text>{item}</Text>
-                    {console.log(item)}
-                    <TouchableOpacity
-                      key={item}
-                      style={styles.removeButton}
-                      onPress={() => handleRemove(item)}
-                    >
-                      <Text>Remove</Text>
-                      {/* {console.log(item)} */
-}
-{
-  /* </TouchableOpacity>
-                  </View>
-                );
-              })
-            : (setList([]), (<Text>add to your history</Text>))} */
-}
