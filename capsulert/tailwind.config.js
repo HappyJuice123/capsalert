52efd349ac9b8f01ac9528ae36b5d38c76014431
holly-th <holly.thomas135@gmail.com>
2023-03-31 17:07:49 +0100

    trail tailwind

---
+++ capsulert/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./App.{js,jsx,ts,tsx}", "./components/SignInPage.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: { rose },
+    screens: {},
+    extend: {},
+  },
+  plugins: [],
+};
