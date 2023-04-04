fc9232752876f1ee4f2364ac7523f580d278163a
Melissa Nzekele Courtney <melissanzekelecourtney@gmail.com>
2023-04-04 22:39:37 +0100

    refactor styling

--- capsulert/components/MyMedicationsItem.jsx
+++ capsulert/components/MyMedicationsItem.jsx
@@ -1,18 +1,18 @@
-import { View, TouchableOpacity, Text } from "react-native";
+import { useState, useContext } from "react";
+import { MedicationsContext } from "../contexts/Medications";
 import { useNavigation } from "@react-navigation/native";
+import { View, TouchableOpacity, Text } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 import { Feather } from "@expo/vector-icons";
 import { Entypo } from "@expo/vector-icons";
-import { Ionicons } from "@expo/vector-icons";
 import { Menu, MenuItem } from "react-native-material-menu";
-import { useState, useContext } from "react";
-import { MedicationsContext } from "../contexts/Medications";
 
 export const MyMedicationsItem = ({
   item,
   handleDelete,
   setModalOpen,
   setEditData,
+  setDetailsModal,
 }) => {
   const [visible, setVisible] = useState(false);
 
@@ -35,62 +35,47 @@ export const MyMedicationsItem = ({
     <View className="bg-whiteGrey p-1">
       <View className="flex flex-row justify-between bg-zinc-50 mx-8 my-4 rounded-md shadow-md shadow-slate-600">
         {/* Medication details */}
-        <View className="text-xs px-5 pt-5 pb-5">
-          <Text className="mb-3 text-lg font-semibold bg-whiteGrey rounded-md py-1 px-12">
-            {item.name}
-          </Text>
-          <Text className="text-xs mb-1">
-            <Text className="font-bold">Brand: </Text>
-            {item.brand}
-          </Text>
-          <Text className="text-xs mb-1">
-            <Text className="font-bold">Start: </Text> {item.startDate}
-          </Text>
-          <Text className="text-xs mb-1">
-            <Text className="font-bold">End: </Text> {item.endDate}
-          </Text>
-          <Text className="text-xs mb-1">
-            <Text className="font-bold">Dosage: </Text>
-            {item.dosage}
-            {item.unit}
-          </Text>
-          <Text className="text-xs mb-1">
-            <Text className="font-bold">Medication Type: </Text>
-            {item.form}
-          </Text>
-          <Text className="text-xs mb-1">
-            <Text className="font-bold">Quantity to take: </Text>
-            {item.quantity}
-          </Text>
-          <TouchableOpacity className="border-2 border-whiteGrey rounded-md mt-3">
-            <Text
-              className="font-semibold text-base text-center"
-              onPress={() => {
-                navigation.navigate("AdditionalMedInfo", item.name);
-              }}
-            >
-              More Info
+        <TouchableOpacity
+          onPress={() => {
+            setDetailsModal(true);
+            setMedicationData(item);
+          }}
+        >
+          <View className="px-4 pt-5 pb-5 w-25">
+            <Text className="font-semibold text-lg bg-whiteGrey rounded-md mb-3 py-1 px-12">
+              {item.name}
             </Text>
-          </TouchableOpacity>
-        </View>
-
-        <TouchableOpacity className="flex flex-row justify-between gap-4 mt-1 mr-3">
-          {/* More info button */}
-          {/* <TouchableOpacity>
-            <Text
-              className="font-semibold"
-              onPress={() => {
-                navigation.navigate("AdditionalMedInfo", item.name);
-              }}
-            >
-              More Info
+            <Text className="text-xs mb-1">
+              <Text className="font-bold">Brand: </Text>
+              {item.brand}
             </Text>
-          </TouchableOpacity> */}
+            <Text className="text-xs mb-1">
+              <Text className="font-bold">Dosage: </Text>
+              {item.dosage}
+              {item.unit}
+            </Text>
+            <Text className="text-xs mb-1">
+              <Text className="font-bold">Medication Type: </Text>
+              {item.form}
+            </Text>
+            <Text className="text-xs mb-1">
+              <Text className="font-bold">Quantity to take: </Text>
+              {item.quantity}
+            </Text>
+            <Text className="text-xs mb-1">
+              <Text className="font-bold">Start Date: </Text> {item.startDate}
+            </Text>
+            <Text className="text-xs mb-1">
+              <Text className="font-bold">End Date: </Text> {item.endDate}
+            </Text>
+          </View>
+        </TouchableOpacity>
 
+        <TouchableOpacity className="flex flex-row justify-between gap-2 mt-3 mr-3">
           {/* Three dot options menu */}
           <TouchableOpacity>
             <Menu
-              className="mt-8 w-25 bg-none rounded-md shadow-md shadow-slate-600"
+              className="mt-8 mr-30 w-25 bg-white rounded-md shadow-md shadow-slate-600"
               visible={visible}
               anchor={
                 <Text onPress={showMenu}>
@@ -103,26 +88,38 @@ export const MyMedicationsItem = ({
                 {/* Edit button */}
 
                 <TouchableOpacity
-                  className="flex flex-row gap-2"
+                  className="flex flex-row my-2"
                   onPress={() => handleEditPress(item)}
                 >
-                  <Text className="font-bold text-sm mr-2"> Edit</Text>
-                  <Feather name="edit" size={22} color="black" />
+                  <Feather name="edit" size={16} color="black" />
+                  <Text className="ml-2"> Edit</Text>
                 </TouchableOpacity>
               </MenuItem>
 
               <MenuItem onPress={hideMenu}>
                 {/* Delete button */}
                 <TouchableOpacity
-                  className="flex flex-row items-center gap-2 mb-5"
+                  className="flex flex-row my-2"
                   onPress={() => handleDelete(item)}
                 >
-                  <Text className="font-bold text-sm"> Delete</Text>
                   <MaterialIcons
                     name="delete-outline"
-                    size={26}
+                    size={22}
                     color="black"
                   />
+                  <Text> Delete</Text>
+                </TouchableOpacity>
+              </MenuItem>
+              <MenuItem onPress={hideMenu}>
+                {/* More info button */}
+                <TouchableOpacity
+                  className="flex flex-row my-2"
+                  onPress={() => {
+                    navigation.navigate("AdditionalMedInfo", item.name);
+                  }}
+                >
+                  <Entypo name="info" size={18} color="black" />
+                  <Text className="ml-2 ">More Info</Text>
                 </TouchableOpacity>
               </MenuItem>
             </Menu>
